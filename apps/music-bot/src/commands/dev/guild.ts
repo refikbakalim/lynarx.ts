a80251c21de38381b87c0b29c22ca87e6e7151ab
refikbakalim <refikbakalim@gmail.com>
2024-02-08 03:26:41 +0300

    Add guild command

---
+++ apps/music-bot/src/commands/dev/guild.ts
@@ -0,0 +1,24 @@
+import type {
+	CommandData,
+	SlashCommandProps,
+	CommandOptions,
+} from "commandkit";
+import { useDatabase } from "#bot/hooks/useDatabase";
+
+export const data: CommandData = {
+	name: "guild",
+	description: "Get guild info from database",
+};
+
+export async function run({ interaction, client }: SlashCommandProps) {
+	const db = useDatabase();
+	const guild = await db.guild
+		.findOne({ id: interaction.guildId })
+		.catch(() => null);
+
+	return interaction.reply(`\`\`\`${guild}\`\`\``);
+}
+
+export const options: CommandOptions = {
+	devOnly: true,
+};
